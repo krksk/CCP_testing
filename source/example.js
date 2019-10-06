a45fadb83b2e84f27b26900e86300bddd8621a57
krksk <cooling343@gmail.com>
2019-10-07 08:36:02 +0900

    6

--- source/example.js
+++ source/example.js
@@ -8,17 +8,8 @@ window.onscroll = function(){
     a4.style.animationPlayState = "running";
     field.style.animationPlayState = "running";
     }
-    if(t>4300){
-    dP_headline.style.animationPlayState = "running";
-    dP_headline_picture.style.animationPlayState = "running";
-    }
-    if(t>6800){
-    dP_headline_2.style.animationPlayState = "running";
-    }
-    if(t>11800){
+    if(t>10300){
     end_headline.style.animationPlayState = "running";
-    end_quote_1.style.animationPlayState = "running";
-    end_quote_2.style.animationPlayState = "running";
     }
     monitor.innerHTML = t + "px";
     }
